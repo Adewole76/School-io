adca311e60061b29a800888b0a672f4277578893
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-08-22 16:06:28 +0100

    ticket 1 completed

--- scripts/module.js
+++ scripts/module.js
@@ -28,3 +28,43 @@ const gettingUser = (name) => {
 const clearSessionStorage = () =>{
     sessionStorage.clear();
 }
+
+const generateId = () => {
+  const timestamp = Date.now().toString(36); 
+  const randomStr = Math.random().toString(36).substring(2, 8);
+  
+  return `${timestamp}-${randomStr}`;
+};
+
+const schoolClasses = [
+    {
+      name:"Js1",
+      id: generateId(),
+      teacherId: null,
+    },
+    {
+      name:"Js2",
+      id: generateId(),
+      teacherId: null,
+    },
+    {
+      name:"Js3",
+      id: generateId(),
+      teacherId: null,
+    },
+    {
+      name:"Ss1",
+      id: generateId(),
+      teacherId: null,
+    },
+    {
+      name:"Ss2",
+      id: generateId(),
+      teacherId: null,
+    },
+    {
+      name:"Ss3",
+      id: generateId(),
+      teacherId: null,
+    },
+]
