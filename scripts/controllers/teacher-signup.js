fcf738327e2da9d9c58382a3baa0ea5601234385
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-08-28 21:39:25 +0100

    teacher-sign up page styling complete

---
+++ scripts/controllers/teacher-signup.js
@@ -0,0 +1,7 @@
+import { addTeacher } from "../module.js";
+import { TeachersArray } from "../module.js";
+const nameInput = document.querySelector('.name-input');
+const emailInput = document.querySelector('.email-input');
+const selectSubject = document.querySelector('.select-subject');
+const passwordInput = document.querySelector('.password-input');
+const confirmPassword = document.querySelector('.confirm-password-input');
