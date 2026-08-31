842ea92a9ec200e39dbad0c4709edd8cba54a8eb
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-08-31 12:09:45 +0100

    route guard added to admin-manage-students and teachers pages

---
+++ scripts/controllers/admin-manage-students.js
@@ -0,0 +1,9 @@
+import { adminArray } from "../module.js";
+import { gettingUser } from "../module.js";
+import { requireAuth } from "../module.js";
+import { clearSessionStorage } from "../module.js";
+const currentUser = gettingUser('currentUserId');
+console.log(currentUser);
+console.log(typeof adminArray);
+const currentUserRole = gettingUser('currentUserRole');
+requireAuth(currentUser, "id", adminArray, currentUserRole, 'admin');
