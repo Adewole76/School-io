a0d73c28b9df14b9b078468b883d39786b272a2a
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-09-02 16:09:26 +0100

    forgot password and password reset flow now fully functional

--- scripts/controllers/reset-password.js
+++ scripts/controllers/reset-password.js
@@ -10,10 +10,10 @@ const confirmPasswordInput = document.querySelector('.confirm-password-input');
 const resetBtn = document.querySelector('.reset-btn');
 const userTochangePassword = getCollection('userTochangePassword');
 
-function hashPassword(password) {
+async function hashPassword(password) {
     const salt = crypto.getRandomValues(new Uint8Array(16));
     const encoder = new TextEncoder();
-    const baseKey = crypto.subtle.importKey(
+    const baseKey = await crypto.subtle.importKey(
       'raw',
       encoder.encode(password),
       'PBKDF2',
@@ -21,7 +21,7 @@ function hashPassword(password) {
       ['deriveBits']
     );
 
-    const hashBuffer = crypto.subtle.deriveBits(
+    const hashBuffer = await crypto.subtle.deriveBits(
       {
         name: 'PBKDF2',
         salt: salt,
@@ -52,6 +52,7 @@ const fifteenMinutes = 3 * 60 * 1000;
    currentTimestamp = Date.now(); 
    if(userTochangePassword){
    let difference = currentTimestamp - userTochangePassword.passwordResetTimestamp;
+   console.log(difference);
    if (difference >= fifteenMinutes){
      userTochangePassword.passwordResetCode = null;
      userTochangePassword.passwordResetTimestamp = null;
@@ -62,33 +63,41 @@ const fifteenMinutes = 3 * 60 * 1000;
  }, 1000);
 
 
-resetBtn.addEventListener('click', function(){
+resetBtn.addEventListener('click', async() => {
     if(!resetCodeInput.value || !resetPasswordInput.value || !confirmPasswordInput.value){
       console.log('please fill in all the fields');
     }else if(resetCodeInput.value && userTochangePassword.passwordResetCode === null){
       console.log('your reset code as expired proceed to the forgot password page to get a new one');
-    }else if(resetCodeInput.value !== userTochangePassword.passwordResetCode && userTochangePassword.passwordResetCode !== null){
+    }else if(Number(resetCodeInput.value) !== userTochangePassword.passwordResetCode && userTochangePassword.passwordResetCode !== null){
+      console.log(typeof resetCodeInput.value);
+      console.log(typeof userTochangePassword.passwordResetCode)
       console.log('you entered the wrong code');
     }else if(resetPasswordInput.value !== confirmPasswordInput.value ){
       console.log('ensure you confirm your password very well');
-    }else if(resetCodeInput.value === userTochangePassword.passwordResetCode && userTochangePassword.passwordResetCode !== null){
+    }else if(Number(resetCodeInput.value) === userTochangePassword.passwordResetCode){
       if(adminArray.some(user => user.email === userTochangePassword.email)){
-        const { newsaltHex, newhashHex } = hashPassword(resetPasswordInput.value);
-        userTochangePassword.saltHex = newsaltHex;
-        userTochangePassword.hashHex = newhashHex;
+        const { saltHex, hashHex } = await hashPassword(resetPasswordInput.value);
+        userTochangePassword.passwordSalt = saltHex;
+        userTochangePassword.passwordHash = hashHex;
         saveCollection('admin', adminArray);
       }else if(studentsArray.some(user => user.Email === userTochangePassword.Email)){
-        const { newsaltHex, newhashHex } = hashPassword(resetPasswordInput.value);
-        userTochangePassword.saltHex = newsaltHex;
-        userTochangePassword.hashHex = newhashHex;
+        const { saltHex, hashHex } = await hashPassword(resetPasswordInput.value);
+        userTochangePassword.passwordSalt = saltHex;
+        userTochangePassword.passwordHash = hashHex;
         saveCollection('students', studentsArray);
       }else if(TeachersArray.some(user => user.Email === userTochangePassword.Email)){
-        const { newsaltHex, newhashHex } = hashPassword(resetPasswordInput.value);
-        userTochangePassword.saltHex = newsaltHex;
-        userTochangePassword.hashHex = newhashHex;
+        const { saltHex, hashHex } = await  hashPassword(resetPasswordInput.value);
+        userTochangePassword.passwordSalt = saltHex;
+        userTochangePassword.passwordHash = hashHex;
+        console.log(userTochangePassword);
+        const TeacherRecordTochange = TeachersArray.find(user => user.Email === userTochangePassword.Email);
+        TeacherRecordTochange.passwordSalt = userTochangePassword.passwordSalt;
+        TeacherRecordTochange.passwordHash = userTochangePassword.passwordHash
+         saveCollection('teachers', TeachersArray);
+         console.log(TeachersArray);
         userTochangePassword.passwordResetCode = null;
         userTochangePassword.passwordResetTimestamp = null
-        saveCollection('teachers', TeachersArray);
+       
       }
   }
 });
