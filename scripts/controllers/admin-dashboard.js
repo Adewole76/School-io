fd419827933dd61f8e218872f313a62df6ccbeb0
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-09-03 10:30:48 +0100

    admin dashboard wired to admin array

--- scripts/controllers/admin-dashboard.js
+++ scripts/controllers/admin-dashboard.js
@@ -3,12 +3,35 @@ import { getCollection } from "../module.js";
 import { adminArray } from "../module.js";
 import { gettingUser } from "../module.js";
 import { requireAuth } from "../module.js";
+import { TeachersArray } from "../module.js"
+import { studentsArray }  from "../module.js"
 import { clearSessionStorage } from "../module.js";
+window.addEventListener('pageshow', (event) => {
+    if (event.persisted) {
+        const freshUserId = gettingUser('currentUserId');
+        const freshUserRole = gettingUser('currentUserRole');
+        requireAuth(freshUserId, "adminid", adminArray, freshUserRole, 'admin');
+    }
+});
 const currentUser = gettingUser('currentUserId');
-console.log(currentUser);
-console.log(typeof adminArray);
 const currentUserRole = gettingUser('currentUserRole');
-requireAuth(currentUser, "id", adminArray, currentUserRole, 'admin');
+console.log(currentUser);
+const currentAdmin = adminArray.find(user => user.adminid === currentUser);
+console.log(typeof adminArray);
+requireAuth(currentUser, "adminid", adminArray, currentUserRole, 'admin');
+const adminName = document.querySelector('.admin-name');
+const totalTeachers = document.querySelector('.total-teachers');
+const totalStudents = document.querySelector('.total-students');
+const unassignedTeachers = document.querySelector('.unassinged-teachers');
+
+adminName.textContent = currentAdmin.name;
+totalTeachers.textContent = TeachersArray.length;
+totalStudents.textContent = studentsArray.length;
+const unassignedTeachersArray = TeachersArray.filter(user => user.ClassId === null);
+unassignedTeachers.textContent = unassignedTeachersArray.length;
+
+
+
 
 const logOut = document.querySelector('.log-out');
 logOut.addEventListener('click', function(){
