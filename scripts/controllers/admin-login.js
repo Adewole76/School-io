20ca88ff4cdaad2d025e1564fa0f8512786fd1e8
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-09-07 19:58:13 +0100

    admin sign in login built

--- scripts/controllers/admin-login.js
+++ scripts/controllers/admin-login.js
@@ -1,6 +1,23 @@
 'use strict'
 import {adminArray} from "../module.js";
 import {verifyPassword} from "../module.js"
+import {saveUserIdOnLogin} from "../module.js"
+import {saveUserRoleOnLogin} from "../module.js"
 const emailInput = document.querySelector('.emailInput');
-const passwordInput = document.querySelector('.passwordInput');\
+const passwordInput = document.querySelector('.passwordInput');
 const signInButton = document.querySelector('.sign-in-btn');
+
+signInButton.addEventListener('click', async() => {
+    if(!emailInput.value || !passwordInput.value){
+        console.log('please fill all necessary Inputs');
+    }else if(adminArray.some(user => user.email === emailInput.value)){
+      let currentAdmin  = adminArray.find(user => user.email === emailInput.value);
+      let isPasswordverified = await verifyPassword(emailInput.value, currentAdmin.passwordSalt, 600000, currentAdmin.passwordHash);
+      if(isPasswordverified){
+        saveUserIdOnLogin(currentAdmin.adminid);
+        saveUserRoleOnLogin('admin')
+        window.location.href = '/pages/admin-dashboard.html';
+      }else {
+        console.log("password is incorrect");
+      };
+}})
