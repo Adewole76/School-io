a337ee3c1b647761c25fd53d9b019b51fe796bdc
Adewole Oluwasegun <adewoleoluwasegun7@gmail.com>
2026-09-21 11:37:49 +0100

    added edit and add student form skeleton to class roster html

--- scripts/controllers/classroster.js
+++ scripts/controllers/classroster.js
@@ -6,6 +6,8 @@ import { saveCollection } from "../module.js";
 import { requireAuth } from "../module.js";
 import { addStudent } from "../module.js";
 import { deleteStudent } from "../module.js";
+import { clearSessionStorage } from "../module.js";
+import { mappingClassRoster } from "../views/classroster-view.js";
 
 const currentUser = gettingUser('currentUserId');
 const currentRole = gettingUser('currentUserRole');
@@ -18,7 +20,12 @@ window.addEventListener('pageshow', (event) => {
     }
 });
 requireAuth(currentUser,"teacherId", TeachersArray, currentRole, "teacher");
-
+//nav bar dom element
+const userName = document.querySelector('.user-name');
+const studentsContainer = document.querySelector('.students-container');
+userName.textContent = currentTeacher.Name; 
 const loadClassRoster = () =>{
     const teachersClassRoster = studentsArray.filter(users => users.Classid === currentTeacher.ClassId);
+    mappingClassRoster(teachersClassRoster, studentsContainer);
 }
+loadClassRoster();
--- scripts/views/classroster-view.js
+++ scripts/views/classroster-view.js
@@ -1,8 +1,26 @@
 'use strict'
-const mappingClassRoster = (arr, container) => {
+export const mappingClassRoster = (arr, container) => {
   const mappedClassRoster = arr.map(user =>{
     return `<div>
-     
+     <div>
+        <h3>${user.Name}</h3>
+        <p></p>
+     </div>
+
+     <ul>
+        <li>Guardian:${user.ParentGuardianNo}</li>
+        <li>Date of birth:${user.dateOfBirth}</li>
+     </ul>
+     <hr>
+     <div>
+        <button>Edit</button>
+        <button>View records</button>
+     </div>
     </div>`
-  })
+  }).join('');
+if(arr.length === 0){
+    container.innerHTML = `<p>No student in your clas yet</p>`
+}else if(arr.length > 0){
+    container.innerHTML = mappedClassRoster;
+}
 }
